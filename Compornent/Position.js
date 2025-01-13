34ea790eb60d6f7a47755ea7533d2263e6a6fd53
kazuya976 <kazu0203d@icloud.com>
2025-01-13 22:56:58 +0900

    スライドパズル作成のためコードを写経中

---
+++ Compornent/Position.js
@@ -0,0 +1,9 @@
+// 正しいコード
+class Position {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+export default Position;
---
+++ Compornent/Stete.js
@@ -0,0 +1,8 @@
+// ゲームの状態を管理
+Class state {
+    constructor(state)
+    this.state = state;
+
+    export default Position;
+
+}
